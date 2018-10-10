4a9642d72b1ff5a9f6d54b5dcfc2be695f81dfa7
Hsiao <hsiaomui@gmail.com>
2018-10-10 16:58:37 +0200

    test

--- tools/DeviceExplorer/DeviceExplorer/Program.cs
+++ tools/DeviceExplorer/DeviceExplorer/Program.cs
@@ -14,6 +14,7 @@ namespace DeviceExplorer
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            //bla
         }
     }
 }
